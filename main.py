6a5c799b26d3946ab0066a7abc089ed5fc384d76
garrettdw304 <gdw00006@mix.wvu.edu>
2024-12-03 20:42:02 -0500

    added mesh and material classes to main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from typing import List, Sequence
 import pygame
 import math
 import time
@@ -5,8 +7,180 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+from PIL import Image
 
 global timeVar  # Time since the beginning of the program
+
+def draw_at(draw_func, posx, posy, posz):
+    glPushMatrix()
+    glTranslate(posx, posy, posz)
+    draw_func()
+    glPopMatrix()
+
+@dataclass
+class Material:
+    specular_exponent : float # Ns
+    ambient_reflection : List[float] # Ka
+    diffused_reflection : List[float] # Kd
+    specular_reflection : List[float] # Ks
+    emissive_material : List[float] # Ke
+    index_of_reflection : float # Ni
+    dissolve_index : float # Dissolve Index (transparency)
+    illum : int
+
+    def bind(self):
+        glMaterial(GL_FRONT, GL_SPECULAR, self.specular_reflection)
+        glMaterial(GL_FRONT, GL_AMBIENT, self.ambient_reflection)
+        glMaterial(GL_FRONT, GL_DIFFUSE, self.diffused_reflection)
+        glMaterial(GL_FRONT, GL_SHININESS, [self.specular_exponent])
+        glMaterial(GL_FRONT, GL_EMISSION, self.emissive_material)
+
+    @staticmethod
+    def load(mtl_file):
+        specular_exponent = 1
+        ambient_reflection = [0, 0, 0]
+        diffused_reflection = [0, 0, 0]
+        specular_reflection = [0, 0, 0]
+        emissive_material = [0, 0, 0]
+        index_of_reflection = 1
+        dissolve_index = 1
+        illum = 2
+        with open(mtl_file, 'r') as mtl:
+            for line in mtl:
+                line = line.strip()
+                if line.startswith("newmtl"): # Expects only one material
+                    continue
+                if line.startswith("#"):
+                    continue
+                if len(line) == 0:
+                    continue
+
+                if line.startswith("Ns"): specular_exponent = float(line.split()[1])
+                elif line.startswith("Ka"): ambient_reflection = [float(line.split()[1]), float(line.split()[2]), float(line.split()[3])]
+                elif line.startswith("Kd"): diffused_reflection = [float(line.split()[1]), float(line.split()[2]), float(line.split()[3])]
+                elif line.startswith("Ks"): specular_reflection = [float(line.split()[1]), float(line.split()[2]), float(line.split()[3])]
+                elif line.startswith("Ke"): emissive_material = [float(line.split()[1]), float(line.split()[2]), float(line.split()[3])]
+                elif line.startswith("Ni"): index_of_reflection = float(line.split()[1])
+                elif line.startswith("d"): dissolve_index = float(line.split()[1])
+                elif line.startswith("illum"): illum = int(line.split()[1])
+                else: print("Cannot parse line of material file: " + line)
+        return Material(specular_exponent, ambient_reflection, diffused_reflection, specular_reflection, emissive_material, index_of_reflection, dissolve_index, illum)
+
+@dataclass
+class Model:
+    default_material = Material(1, [1, 1, 1], [1, 1, 1], [1, 1, 1], [0, 0, 0], 1, 1, 2)
+    # From obj file
+    vertices : List[List[float]] # Vertex Array
+    normals : List[List[float]] # Normals Array
+    uvs : List[List[float]] # Texture Vertex Array
+    faces : List[List[List[int]]] # Faces Array. 1 index to vertices, 2 index to texture coordinates, 3 index to normals
+    # From mtl file
+    material : Material
+    # From .png or similar file
+    texture : Sequence[int] or None
+    texture_id : int # The texture index of where the texture is stored at on the gpu. If not yet passed to gpu, -1.
+
+    # Sends the texture to the GPU and stores the texture id into texture_id. Throws if texture_id is not -1.
+    # When leaving this method, the currently bound texture is this texture
+    def send_texture(self):
+        if self.texture_id != -1: raise Exception("Texture is already in GPU.")
+        if self.texture is None: raise Exception("Cannot send a texture if there is not one to send.")
+        self.texture_id = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture_id)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1024, 1024, 0, GL_RGB, GL_UNSIGNED_BYTE, self.texture) # TODO: 1024 magic number
+
+    def clear_texture(self):
+        if self.texture_id == -1: raise Exception("Texture is not loaded into GPU.")
+        glDeleteTextures(self.texture_id)
+        self.texture_id = -1
+
+    def bind_texture(self):
+        if self.texture_id == -1: raise Exception("Texture is not loaded into GPU.")
+        glBindTexture(GL_TEXTURE_2D, self.texture_id)
+
+    def unbind_texture(self):
+        if self.texture_id == -1: raise Exception("Texture cannot be bound if it is not loaded into GPU...")
+        glBindTexture(GL_TEXTURE_2D, 0)
+
+    @staticmethod
+    def load(obj_file, texture_file = None):
+        vertices = []
+        normals = []
+        uvs = []
+        faces = []
+        material = Model.default_material
+        with open(obj_file, 'r') as mtl:
+            for line in mtl:
+                line = line.strip()
+                if len(line) == 0:
+                    continue
+                if line.startswith("#"):
+                    continue
+                if line.startswith("o"): # Expects only one object
+                    continue
+                if line.startswith("s"): # Smooth shading
+                    continue
+                if line.startswith("usemtl"): # Expects only one material
+                    continue
+
+                split = line.split(' ')
+
+                if line.startswith("mtllib"):
+                    material = Material.load('/'.join(obj_file.split('/')[:-1]) + "/" + ' '.join(split[1:])) # Probably a terrible solution
+                elif line.startswith("vn"):
+                    normals.append([float(split[1]), float(split[2]), float(split[3])])
+                elif line.startswith("vt"):
+                    uvs.append([float(split[1]), float(split[2])])
+                elif line.startswith("v"):
+                    vertices.append([float(split[1]), float(split[2]), float(split[3])])
+                elif line.startswith("f"):
+                    face = []
+                    for vert in line.split()[1:]:
+                        indices = vert.split('/')
+                        face.append([int(indices[0]) - 1, int(indices[1]) - 1, int(indices[2]) - 1]) # - 1 because indices start at 1 not 0
+                    faces.append(face)
+                else:
+                    print("Cannot parse line of obj file: " + line)
+        texture = None
+        if texture_file is not None:
+            texture = Image.open(texture_file).transpose(Image.Transpose.FLIP_TOP_BOTTOM).convert("RGB").tobytes()
+        return Model(vertices, normals, uvs, faces, material, texture, -1)
+
+def draw_model(model : Model):
+    if model.texture is not None:
+        model.bind_texture()
+    model.material.bind()
+    length = -1  # -1 -> glBegin has not been called, 0 -> drawing polygons, 3 -> drawing triangles, 4 -> drawing quads
+    for face in model.faces:
+        if len(face) != length:
+            if length != -1:
+                glEnd()
+            length = len(face)
+            if length != 3 or length != 4:
+                length = 0
+            if length == 3:
+                glBegin(GL_TRIANGLES)
+            elif length == 4:
+                glBegin(GL_QUADS)
+            else:
+                glBegin(GL_POLYGON)
+        elif length == 0:
+            glEnd()
+            glBegin(GL_POLYGON)
+
+        for indices in face:  # Each 'indices' contains 3 vertex indices: 0 -> mesh vertex index, 1 -> texture vertex index, 2 -> normal vertex index
+            glNormal3fv(model.normals[indices[2]])
+            if model.texture is not None:
+                glTexCoord2fv(model.uvs[indices[1]])
+            glVertex3fv(model.vertices[indices[0]])
+    if length != -1:
+        glEnd()
+    if model.texture is not None:
+        model.unbind_texture()
 
 def cube(xSize, ySize, zSize):
     glBegin(GL_POLYGON)
